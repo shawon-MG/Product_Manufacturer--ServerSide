070324ea51bf67dd2b724be5b54f193cbd060b44
shawon-MG <work.shawomg@gmail.com>
2022-05-25 18:58:25 +0600

    Getting all products-->app.get('/products')

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 require('dotenv').config();
 
 const cors = require('cors');
@@ -9,6 +12,33 @@ const cors = require('cors');
 // MiddleWare :
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.xd9lnfn.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+
+        await client.connect();
+        const productCollection = client.db('final-project').collection('products');
+
+        // Getting all Products: (Shown at home page) 
+        app.get('/products', async (req, res) => {
+            const products = await productCollection.find({}).toArray();
+
+            res.send(products);
+        });
+
+    }
+    finally {
+        // no code for now...
+    }
+
+};
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
