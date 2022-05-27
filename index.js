d148b37a6f6dc5d783810f3304912e87539c402c
shawon-MG <work.shawomg@gmail.com>
2022-05-27 11:31:59 +0600

    profile information sended

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
         const productCollection = client.db('final-project').collection('products');
         const reviewCollection = client.db('final-project').collection('reviews');
         const purchaseDataCollection = client.db('final-project').collection('purchaseProducts');
+        const profileDataCollection = client.db('final-project').collection('profiles');
 
         // Getting all Products: (Shown at home page) 
         app.get('/products', async (req, res) => {
@@ -65,8 +66,6 @@ async function run() {
             res.send(result);
         });
 
-
-
         app.get('/purchase/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -90,6 +89,23 @@ async function run() {
 
             res.send(result);
         });
+
+
+
+        // Posting User's detail profile information to profileData collection in mongoDB
+        app.post('/profile-info', async (req, res) => {
+            const profileData = req.body;
+            const result = await profileDataCollection.insertOne(profileData);
+
+            res.send(result);
+        });
+        // Getting user profile information : ( Not shown at UI. But can be seen at localhoast of the server side)
+        app.get('/profile-info', async (req, res) => {
+            const result = await profileDataCollection.find({}).toArray();
+
+            res.send(result);
+        });
+
 
     }
     finally {
