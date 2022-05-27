ee478b38715479810eff9b6c60a00faef279da32
shawon-MG <work.shawomg@gmail.com>
2022-05-27 18:58:14 +0600

    admin creation from all users done (some code updated)

--- index.js
+++ index.js
@@ -46,6 +46,18 @@ async function run() {
             res.send(allUsers);
         });
 
+        // Making a user Admin from all users: 
+        app.put('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+        });
+
         // Getting all Products: (Shown at home page) 
         app.get('/products', async (req, res) => {
             const products = await productCollection.find({}).toArray();
