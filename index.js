59d814308f146ed403e811e4d358716c359edc72
shawon-MG <work.shawomg@gmail.com>
2022-05-28 16:32:42 +0600

    Error handled

--- index.js
+++ index.js
@@ -83,9 +83,9 @@ async function run() {
 
         app.get('/users/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const user = await userCollection.findOne({ email: email });
+            const user = await userCollection.findOne({ email: email }) || {};
             if (user) {
-                const isAdmin = user.role === 'admin';
+                const isAdmin = user?.role === 'admin';
             }
 
             res.send({ admin: isAdmin });
